8da6886af4d30140bc8aabf177f20e066db9984d
Antonette Caldwell <pullmana8@gmail.com>
2020-03-28 13:35:51 -0500

    API backend

---
+++ udagram-api/src/config/config.ts
@@ -0,0 +1,12 @@
+export const config = {
+  "dev": {
+    "username": "udagramcalddev",
+    "password": "DecemberIsFreezing2019!",
+    "database": "example_db",
+    "host": "udagramcaldwell-dev.ccnrhqkrquu9.us-east-2.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-caldwell-dev"
+  }
+}

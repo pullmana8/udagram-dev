e8006fcf5245eb8975ed734c43a84da9e76eb41e
Antonette Caldwell <pullmana8@gmail.com>
2020-04-03 14:31:24 -0500

    Remove sensitive information

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,17 +1,17 @@
 export const config = {
   "dev": {
-    "username": "udagramcalddev",
-    "password": "DecemberIsFreezing2019!",
-    "database": "example_db",
-    "host": "udagramcaldwell-dev.ccnrhqkrquu9.us-east-2.rds.amazonaws.com",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DB,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
   },
   "aws": {
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-caldwell-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_BUCKET
   },
   "jwt": {
-    "secret": "helloworld"
+    "secret": process.env.JWT_SECRET
   }
 }

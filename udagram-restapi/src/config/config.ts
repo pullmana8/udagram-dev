63e7881156f59d208d4faadb31f442058898cb8b
Antonette Caldwell <pullmana8@gmail.com>
2020-04-03 16:01:11 -0500

    Renamed udagram-api to udagram-restapi

---
+++ udagram-restapi/src/config/config.ts
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DB,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": "postgres",
+  },
+  "aws": {
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
+  }
+}
